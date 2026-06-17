73e4284de706b63debf4a0715ceebb4b1fbe7647
zartabkhan <zartabkhan2008@gmail.com>
2026-06-17 11:47:47 +0530

    a

---
+++ 01 Frontend/4 front-end-javascript/2026/02 Practice/10 ES6/01 let,const,var Diffrent.js
@@ -0,0 +1,45 @@
+// Topic (1) => 5 Different in let,const,var
+
+// diffrent (1)
+var x = 10;
+var x = 20; 
+
+let y = 10;
+// let y = 20;  
+
+const z = 10;
+// const z = 20; 
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// diffrent (2)
+var a = 
+a = 49 
+
+let b = 10
+b = 113 
+
+const c = 10
+// c = 20
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// if (true) {
+//   var m = 10;
+//   let z = 20;
+//   const n = 30;
+// }
+
+// console.log(m); 
+// console.log(z); 
+// console.log(n); 
+
+// console.log(a); 
+// var a = 10;
+
+// console.log(b); 
+// let b = 20;
